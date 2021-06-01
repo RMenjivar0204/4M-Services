0fbc3507640a11acc8e509e21b9aebc352d8d9d7
Raul Menjivar <raul.menjivar4@gmail.com>
2021-06-01 10:36:14 -0500

    basic website template setup

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+
+app.listen(3000, function(){
+  console.log("Now listening on Port 3000");
+});
